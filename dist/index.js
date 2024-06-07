dca6bc730d72be6042201f75dadbbf02a42f9d19
Raghvendra Awasthi <raghvendra@biz4group.com>
2024-06-07 19:17:33 +0530

    Removed style import from dist

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,24 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
+exports["default"] = exports.ReactSmartTable = void 0;
 var React = _interopRequireWildcard(require("react"));
-require("./style.css");
-var _excluded = ["currentPage", "customLoader", "hasMoreRecords", "headings", "inverseScroll", "items", "loading", "loadMore", "onPageChange", "onRowClick", "recordsView", "recordsPerPage", "scopedFields", "totalPages"],
+var _excluded = ["currentPage", "customLoader", "hasMoreRecords", "headings", "inverseScroll", "items", "loading", "loadMore", "onPageChange", "onRowClick", "parentClass", "recordsView", "recordsPerPage", "scopedFields", "totalPages"],
   _excluded2 = ["title", "fieldName"];
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
+function _extends() { return _extends = Object.assign ? Object.assign.bind() : function (n) { for (var e = 1; e < arguments.length; e++) { var t = arguments[e]; for (var r in t) ({}).hasOwnProperty.call(t, r) && (n[r] = t[r]); } return n; }, _extends.apply(null, arguments); }
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
-function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i["return"] && (_r = _i["return"](), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
+function _objectWithoutProperties(e, t) { if (null == e) return {}; var o, r, i = _objectWithoutPropertiesLoose(e, t); if (Object.getOwnPropertySymbols) { var n = Object.getOwnPropertySymbols(e); for (r = 0; r < n.length; r++) o = n[r], t.indexOf(o) >= 0 || {}.propertyIsEnumerable.call(e, o) && (i[o] = e[o]); } return i; }
+function _objectWithoutPropertiesLoose(r, e) { if (null == r) return {}; var t = {}; for (var n in r) if ({}.hasOwnProperty.call(r, n)) { if (e.indexOf(n) >= 0) continue; t[n] = r[n]; } return t; }
 function ReactSmartTableComponent(_ref) {
   var _Array$fill;
   var currentPage = _ref.currentPage,
@@ -33,6 +32,8 @@ function ReactSmartTableComponent(_ref) {
     loadMore = _ref.loadMore,
     onPageChange = _ref.onPageChange,
     onRowClick = _ref.onRowClick,
+    _ref$parentClass = _ref.parentClass,
+    parentClass = _ref$parentClass === void 0 ? "scrollable-area" : _ref$parentClass,
     recordsView = _ref.recordsView,
     recordsPerPage = _ref.recordsPerPage,
     scopedFields = _ref.scopedFields,
@@ -47,6 +48,7 @@ function ReactSmartTableComponent(_ref) {
     _React$useState2 = _slicedToArray(_React$useState, 2),
     element = _React$useState2[0],
     setElement = _React$useState2[1];
+  var tableRowRef = React.useRef(null);
   var itemsRef = React.useRef(items);
   var hasMoreRecordsRef = React.useRef(hasMoreRecords);
   var scrollObserver = React.useRef(new IntersectionObserver(function (_ref2) {
@@ -56,7 +58,7 @@ function ReactSmartTableComponent(_ref) {
       loadMore();
     }
   }, {
-    threshold: 1
+    threshold: 0.7
   }));
 
   // Observing the chat messages container movement
@@ -80,24 +82,33 @@ function ReactSmartTableComponent(_ref) {
     if (items.length) {
       itemsRef.current = items;
     }
-  }, [items, items.length]);
+    if (inverseScroll) {
+      var _tableRowRef$current;
+      (_tableRowRef$current = tableRowRef.current) === null || _tableRowRef$current === void 0 || _tableRowRef$current.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  }, [items, items.length, inverseScroll]);
   /** Configuration for Infinite Scroll Ends */
 
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
-    className: "scrollable-area"
-  }, recordsView === "infinite-Scroll" && inverseScroll && items.length && /*#__PURE__*/React.createElement("p", {
-    style: {
-      color: "black"
-    },
-    ref: setElement
-  }, "Loading..."), /*#__PURE__*/React.createElement("table", props, headings && headings.length ? /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, headings.map(function (_ref4, i) {
+    className: parentClass
+  }, /*#__PURE__*/React.createElement("table", props, headings && headings.length ? /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, headings.map(function (_ref4, i) {
     var title = _ref4.title,
       fieldName = _ref4.fieldName,
       restAttr = _objectWithoutProperties(_ref4, _excluded2);
     return /*#__PURE__*/React.createElement("th", _extends({
       key: i
     }, restAttr), title !== null && title !== void 0 ? title : fieldName);
-  }))) : null, /*#__PURE__*/React.createElement("tbody", null, items && items.length ? items.map(function (item, itemKey) {
+  }))) : null, /*#__PURE__*/React.createElement("tbody", null, recordsView === "infinite-Scroll" && inverseScroll && items.length && /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+    colSpan: headings.length
+  }, /*#__PURE__*/React.createElement("p", {
+    style: {
+      color: "black",
+      textAlign: "center"
+    },
+    ref: setElement
+  }, customLoader !== null && customLoader !== void 0 ? customLoader : "Loading..."))), items && items.length ? items.map(function (item, itemKey) {
     return /*#__PURE__*/React.createElement("tr", {
       key: itemKey,
       onClick: function onClick() {
@@ -114,12 +125,15 @@ function ReactSmartTableComponent(_ref) {
     }));
   }) : loading ? /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
     colSpan: fields.length
-  }, customLoader !== null && customLoader !== void 0 ? customLoader : "Loading")) : null)), recordsView === "infinite-Scroll" && !inverseScroll && items.length && /*#__PURE__*/React.createElement("p", {
+  }, customLoader !== null && customLoader !== void 0 ? customLoader : "Loading...")) : null, recordsView === "infinite-Scroll" && !inverseScroll && items.length && /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+    colSpan: headings.length
+  }, /*#__PURE__*/React.createElement("p", {
     style: {
-      color: "black"
+      color: "black",
+      textAlign: "center"
     },
     ref: setElement
-  }, "Loading...")), /*#__PURE__*/React.createElement("div", {
+  }, customLoader !== null && customLoader !== void 0 ? customLoader : "Loading...")))))), /*#__PURE__*/React.createElement("div", {
     className: "page-bar"
   }, totalPages && currentPage ? /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("span", {
     onClick: function onClick() {
@@ -137,6 +151,7 @@ function ReactSmartTableComponent(_ref) {
         return onPageChange && onPageChange(currentPage === 1 ? currentPage : currentPage - 1);
       }
     }, "<") : null, /*#__PURE__*/React.createElement("span", {
+      className: currentPage === i + 1 ? "actve-page" : "",
       onClick: function onClick() {
         return onPageChange && onPageChange(i + 1);
       }
@@ -151,5 +166,5 @@ function ReactSmartTableComponent(_ref) {
     }
   }, ">>")) : null));
 }
-var _default = ReactSmartTableComponent;
-exports["default"] = _default;
+var ReactSmartTable = exports.ReactSmartTable = ReactSmartTableComponent;
+var _default = exports["default"] = ReactSmartTableComponent;
